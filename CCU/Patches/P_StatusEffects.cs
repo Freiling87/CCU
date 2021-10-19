f244063be359aadec5bdb10930f2cdf3e9edd89b
FN LN <lucas87t@gmail.com>
2021-10-19 15:50:38 -0500

    Attempted Explode On Death

---
+++ CCU/Patches/P_StatusEffects.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx.Logging;
+using CCU.Traits.AI.Behavior;
+using HarmonyLib;
+using RogueLibsCore;
+
+namespace CCU.Patches
+{
+	[HarmonyPatch(declaringType:typeof(StatusEffects))]
+	class P_StatusEffects
+	{
+		private static readonly ManualLogSource logger = CCULogger.GetLogger();
+		public static GameController GC => GameController.gameController;
+
+		[HarmonyPostfix, HarmonyPatch(methodName:nameof(StatusEffects.ExplodeAfterDeathChecks))]
+		public static void ExplodeAfterDeathChecks_Postfix(StatusEffects __instance, ref bool ___result)
+		{
+			if (__instance.agent.HasTrait<Behavior_ExplodeOnDeath>())
+				___result = true;
+		}
+	}
+}
