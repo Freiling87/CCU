3875803fde041cd60e61dfd656952502426b4fbc
FN LN <lucas87t@gmail.com>
2022-09-24 12:24:01 -0500

    T - Character Creation screen UI changes: CCU Trait section, vanilla trait auto-sorting,

---
+++ CCU/Patches/Interface/P_CharacterCreation.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx.Logging;
+using BTHarmonyUtils.TranspilerUtils;
+using CCU.Traits;
+using HarmonyLib;
+using RogueLibsCore;
+using UnityEngine.UI;
+
+namespace CCU.Patches.Inventory
+{
+	[HarmonyPatch(declaringType: typeof(CharacterCreation))]
+	public static class P_CharacterCreation
+	{
+		private static readonly ManualLogSource logger = CCULogger.GetLogger();
+		public static GameController GC => GameController.gameController;
+
+		[HarmonyTranspiler, HarmonyPatch(methodName: nameof(CharacterCreation.CreatePointTallyText))]
+		private static IEnumerable<CodeInstruction> CreatePointTallyText_FilterCCPCount(IEnumerable<CodeInstruction> codeInstructions)
+		{
+			List<CodeInstruction> instructions = codeInstructions.ToList();
+			FieldInfo traitsChosen = AccessTools.DeclaredField(typeof(CharacterCreation), nameof(CharacterCreation.traitsChosen));
+			MethodInfo playerUnlockList = AccessTools.DeclaredMethod(typeof(T_CCU), nameof(T_CCU.PlayerUnlockList), parameters: new Type[] { typeof(List<Unlock>) });
+			MethodInfo autoSortTraitsChosen = AccessTools.DeclaredMethod(typeof(P_CharacterCreation), nameof(P_CharacterCreation.AutoSortTraitsChosen));
+
+			CodeReplacementPatch patch = new CodeReplacementPatch(
+				expectedMatches: 2,
+				prefixInstructionSequence: new List<CodeInstruction>
+				{
+					new CodeInstruction(OpCodes.Ldarg_0),
+					new CodeInstruction(OpCodes.Ldfld, traitsChosen)
+					
+				},
+				targetInstructionSequence: new List<CodeInstruction>
+				{
+					
+				},
+				insertInstructionSequence: new List<CodeInstruction>
+				{
+					new CodeInstruction(OpCodes.Call, playerUnlockList),
+					new CodeInstruction(OpCodes.Ldarg_0),
+					new CodeInstruction(OpCodes.Call, autoSortTraitsChosen),
+				},
+				postfixInstructionSequence: new List<CodeInstruction>
+				{
+					new CodeInstruction(OpCodes.Callvirt),
+					new CodeInstruction(OpCodes.Stloc_S, 12),
+				});
+
+			patch.ApplySafe(instructions, logger);
+			return instructions;
+		}
+		private static void AutoSortTraitsChosen(CharacterCreation characterCreation)
+        {
+			if (characterCreation.sortByName)
+				characterCreation.traitsChosen = T_CCU.AlphabetizeUnlockList(characterCreation.traitsChosen);
+			else
+				characterCreation.traitsChosen = T_CCU.SortUnlockListByCCP(characterCreation.traitsChosen);
+        }
+
+		[HarmonyTranspiler, HarmonyPatch(methodName: nameof(CharacterCreation.CreatePointTallyText))]
+		private static IEnumerable<CodeInstruction> CreatePointTallyText_CustomCCUSection(IEnumerable<CodeInstruction> codeInstructions)
+		{
+			List<CodeInstruction> instructions = codeInstructions.ToList();
+			FieldInfo itemsChosen = AccessTools.DeclaredField(typeof(CharacterCreation), nameof(CharacterCreation.itemsChosen));
+			MethodInfo printTraitList = AccessTools.DeclaredMethod(typeof(P_CharacterCreation), nameof(P_CharacterCreation.PrintTraitList));
+
+			CodeReplacementPatch patch = new CodeReplacementPatch(
+				expectedMatches: 1,
+				prefixInstructionSequence: new List<CodeInstruction>
+				{
+					new CodeInstruction(OpCodes.Ldarg_0),
+				},
+				targetInstructionSequence: new List<CodeInstruction>
+				{
+
+				},
+				insertInstructionSequence: new List<CodeInstruction>
+				{
+					new CodeInstruction(OpCodes.Call, printTraitList),
+					new CodeInstruction(OpCodes.Ldarg_0), // Replace original
+				},
+				postfixInstructionSequence: new List<CodeInstruction>
+				{
+					new CodeInstruction(OpCodes.Ldfld, itemsChosen),
+					new CodeInstruction(OpCodes.Callvirt),
+					new CodeInstruction(OpCodes.Ldc_I4_0),
+					new CodeInstruction(OpCodes.Ble_S),
+				});
+
+			patch.ApplySafe(instructions, logger);
+			return instructions;
+		}
+		public static void PrintTraitList(CharacterCreation characterCreation)
+        {
+			if (T_CCU.DesignerUnlockList(characterCreation.traitsChosen).Any())
+				characterCreation.pointTallyText.text += "\n<color=red>- CCU TRAITS -</color>\n";
+			else
+				return;
+
+			foreach (Unlock unlock in T_CCU.AlphabetizeUnlockList(T_CCU.DesignerUnlockList(characterCreation.traitsChosen)))
+				characterCreation.pointTallyText.text += GC.nameDB.GetName(unlock.unlockName, "StatusEffect") + "\n";
+		}
+
+		// TODO: This was to manage trait maximums. It can be ignored for now.
+
+		//[HarmonyPostfix, HarmonyPatch(methodName: nameof(CharacterCreation.AddToList), argumentTypes: new[] { typeof(string), typeof(string) })]
+		//public static void AddToList_Postfix(string listType, string unlockName, CharacterCreation __instance)
+		//{
+		//	Core.LogMethodCall();	
+		//	logger.LogDebug("\tlistType: " + listType + "\n\tunlockName: " + unlockName);
+
+		//	if (listType == "Traits" && TraitManager.AllCCUTraitNamesGroup.Contains(unlockName))
+		//		__instance.traitLimit += 1;
+		//}
+
+		//[HarmonyPostfix, HarmonyPatch(methodName: nameof(CharacterCreation.RemoveFromList), argumentTypes: new[] { typeof(string), typeof(string) })]
+		//public static void RemoveFromList_Postfix(string listType, string unlockName, CharacterCreation __instance)
+		//{
+		//	Core.LogMethodCall();
+		//	logger.LogDebug("\tlistType: " + listType + "\n\tunlockName: " + unlockName);
+
+		//	if (listType == "Traits" && TraitManager.AllCCUTraitNamesGroup.Contains(unlockName))
+		//		__instance.traitLimit -= 1;
+		//}
+	}
+}
--- CCU/Traits/T_CCU.cs
+++ CCU/Traits/T_CCU.cs
@@ -1,4 +1,5 @@
-﻿using RogueLibsCore;
+﻿using BepInEx.Logging;
+using RogueLibsCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,9 @@ namespace CCU.Traits
 {
     public abstract class T_CCU : CustomTrait
     {
+        private static readonly ManualLogSource logger = CCULogger.GetLogger();
+        public static GameController GC => GameController.gameController;
+
         public static TraitBuilder PostProcess
         {
             // This is applied after each trait is created. I don't fully get how it works, but see where PP's value is assigned.
@@ -18,13 +22,37 @@ namespace CCU.Traits
             }
         }
 
-        // Remove all CCU traits that aren't Player traits
-        public static List<Trait> DisplayableTraits(List<Trait> vanilla) =>
-            vanilla.Where(t => IsTraitDisplayable(t)).ToList();
+        public static List<Unlock> AlphabetizeUnlockList(List<Unlock> original) =>
+            original.OrderBy(u => GC.nameDB.GetName(u.unlockName, "StatusEffect")).ToList();
 
-        public static bool IsTraitDisplayable(Trait trait) =>
+        public static List<Trait> DesignerTraitList(List<Trait> original) =>
+            original.Where(t => IsDesignerTrait(t)).ToList();
+        public static List<Unlock> DesignerUnlockList(List<Unlock> original) =>
+            original.Where(u => IsDesignerUnlock(u)).ToList();
+
+        public static List<Trait> PlayerTraitList(List<Trait> original) =>
+            original.Where(t => IsPlayerTrait(t)).ToList();
+        public static List<Unlock> PlayerUnlockList(List<Unlock> original) =>
+            original.Where(u => IsPlayerUnlock(u)).ToList();
+
+        public static List<Unlock> SortUnlockListByCCP(List<Unlock> original) =>
+            original.OrderBy(u => u.cost3).ToList();
+
+        public static List<Unlock> VanillaTraitList(List<Unlock> original) =>
+            original.Where(u => !(u.GetHook() is TraitUnlock_CCU)).ToList();
+
+        public static bool IsDesignerTrait(Trait trait) =>
+            !(trait?.GetHook<T_CCU>() is null) &&
+            trait?.GetHook<T_PlayerTrait>() is null;
+        public static bool IsDesignerUnlock(Unlock unlock) =>
+            unlock.GetHook() is TraitUnlock_CCU traitUnlock_CCU && !traitUnlock_CCU.PlayerTrait;
+
+        public static bool IsPlayerTrait(Trait trait) =>
             trait?.GetHook<T_CCU>() is null ||
             !(trait?.GetHook<T_PlayerTrait>() is null);
+        public static bool IsPlayerUnlock(Unlock unlock) =>
+            !(unlock.GetHook() is TraitUnlock_CCU) ||
+            (unlock.GetHook() is TraitUnlock_CCU traitUnlock_CCU && traitUnlock_CCU.PlayerTrait);
 
         public string TextName => 
             DesignerName(GetType());
