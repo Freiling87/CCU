bd4a2035a5f4cca6fe8a0a396224cc9fd1175e38
FN LN <lucas87t@gmail.com>
2023-06-19 14:27:59 -0500

    T - Added Indomitable

---
+++ CCU/Traits/Passive/Indomitable.cs
@@ -0,0 +1,36 @@
+﻿using RogueLibsCore;
+using System;
+
+namespace CCU.Traits.Passive
+{
+    public class Indomitable : T_CCU, ISetupAgentStats
+    {
+        [RLSetup]
+        public static void Setup()
+        {
+            PostProcess = RogueLibs.CreateCustomTrait<Indomitable>()
+                .WithDescription(new CustomNameInfo
+                {
+                    [LanguageCode.English] = String.Format("Immune to mind control."),
+                })
+                .WithName(new CustomNameInfo
+                {
+                    [LanguageCode.English] = DesignerName(typeof(Indomitable)),
+                })
+                .WithUnlock(new TraitUnlock_CCU
+                {
+                    CharacterCreationCost = 0,
+                    IsAvailable = false,
+                    IsAvailableInCC = Core.designerEdition,
+                    UnlockCost = 0,
+                });
+        }
+        public override void OnAdded() { }
+        public override void OnRemoved() { }
+
+		public void SetupAgentStats(Agent agent)
+		{
+            agent.preventsMindControl = true;
+		}
+	}
+}
