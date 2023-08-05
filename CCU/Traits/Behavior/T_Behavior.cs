dcbeda6e714e3ffaa9964782ba334dd82dc5cd53
FN LN <lucas87t@gmail.com>
2023-08-05 14:03:28 -0500

    Added Vigilance traits

--- CCU/Traits/Behavior/T_Behavior.cs
+++ CCU/Traits/Behavior/T_Behavior.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections.Generic;
-
-namespace CCU.Traits.Behavior
+﻿namespace CCU.Traits.Behavior
 {
-    public abstract class T_Behavior : T_CCU
+	public abstract class T_Behavior : T_CCU
     {
         public T_Behavior() : base() { }
 
+        // Move to a LOSCheckTrait interface
         public abstract bool LosCheck { get; }
         public abstract string[] GrabItemCategories { get; }
     }
---
+++ CCU/Traits/Behavior/Vigilance/Vigilanter.cs
@@ -0,0 +1,39 @@
+﻿using RogueLibsCore;
+
+namespace CCU.Traits.Behavior
+{
+	public class Vigilanter : T_Behavior, ISetupAgentStats
+    {
+        public override bool LosCheck => false;
+        public override string[] GrabItemCategories => new string[] { };
+
+        [RLSetup]
+        public static void Setup()
+        {
+            PostProcess = RogueLibs.CreateCustomTrait<Vigilanter>()
+                .WithDescription(new CustomNameInfo
+                {
+                    [LanguageCode.English] = "Reacts to sound like Bartender, Bouncer, Cannibal & Goon.",
+                })
+                .WithName(new CustomNameInfo
+                {
+                    [LanguageCode.English] = DesignerName(typeof(Vigilanter)),
+                })
+                .WithUnlock(new TraitUnlock_CCU
+                {
+                    Cancellations = { },
+                    CharacterCreationCost = 0,
+                    IsAvailable = false,
+                    IsAvailableInCC = Core.designerEdition,
+                    UnlockCost = 0,
+                });
+        }
+        public override void OnAdded() { }
+        public override void OnRemoved() { }
+
+        public void SetupAgentStats(Agent agent)
+        {
+            agent.modVigilant = 2;
+        }
+    }
+}
